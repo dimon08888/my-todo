9aca2c8790dcd7a7b035618a12f08f78c55be0e1
dimon08888 <dimon08888@mail.ru>
2022-06-12 20:24:56 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,159 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Todos = {
+  id: string
+  text: string
+  completed: boolean
+  edit: boolean
 }
 
-export default App;
+function App() {
+  const [todos, setTodos] = useState<Todos[]>([])
+  const [filterTodos, setFilterTodos] = useState<Todos[]>([])
+  const [text, setText] = useState('')
+  const [changeTodo, setChangeTodo] = useState('')
+
+  useEffect(() => {
+    setFilterTodos(todos)
+  }, [todos])
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        { id: new Date().toISOString(), text, completed: false, edit: false },
+      ])
+      setText('')
+    }
+  }
+
+  function onDelete(id: string) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function ontoggleChecked(id: string) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id !== id) return todo
+        return {
+          ...todo,
+          completed: !todo.completed,
+        }
+      })
+    )
+  }
+
+  function handleAllFilter() {
+    const newArr = [...todos]
+    setFilterTodos(newArr)
+  }
+
+  function handleActiveFilter() {
+    const newArr = [...todos].filter(todo => !todo.completed)
+    setFilterTodos(newArr)
+  }
+
+  function handleCompletedFilter() {
+    const newArr = [...todos].filter(todo => todo.completed)
+    setFilterTodos(newArr)
+  }
+
+  function handleMarkAll() {
+    setTodos(
+      todos.map(todo => ({
+        ...todo,
+        completed: !todo.completed,
+      }))
+    )
+  }
+
+  function handleClearCompleted() {
+    setTodos(todos.filter(todo => !todo.completed))
+  }
+
+  function handleEditTodo(todoId: string) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id !== todoId) return todo
+        return {
+          ...todo,
+          edit: !todo.edit,
+        }
+      })
+    )
+  }
+
+  function onChangeTodo(todoId: string) {
+    if (changeTodo.length) {
+      setTodos(
+        todos.map(todo => {
+          if (todo.id !== todoId) return todo
+          return {
+            ...todo,
+            text: changeTodo,
+          }
+        })
+      )
+    }
+    setChangeTodo('')
+  }
+
+  return (
+    <div className='App'>
+      <h1>What needs to be done?</h1>
+
+      <form onSubmit={onSubmit}>
+        <label>
+          <input type='text' onChange={e => setText(e.target.value)} value={text} />
+        </label>
+        <button>Add #{todos.length + 1}</button>
+      </form>
+
+      <div className='filters'>
+        <button onClick={() => handleAllFilter()}>All</button>
+        <button onClick={() => handleActiveFilter()}>Active</button>
+        <button onClick={() => handleCompletedFilter()}>Completed</button>
+      </div>
+
+      <div className='remaining-info'>
+        {todos.length} {todos.length === 1 ? 'task to remaining' : 'tasks to remaining'}
+      </div>
+
+      <ul>
+        {filterTodos.map(todo => (
+          <li key={todo.id}>
+            <input
+              type='checkbox'
+              checked={todo.completed}
+              onChange={() => ontoggleChecked(todo.id)}
+            />
+            <span>{todo.text}</span>
+            <button onClick={() => handleEditTodo(todo.id)}>Edit</button>
+            <button className='delete' onClick={() => onDelete(todo.id)}>
+              &times;
+            </button>
+            {todo.edit && (
+              <div>
+                <input
+                  type='text'
+                  value={changeTodo}
+                  onChange={e => setChangeTodo(e.target.value)}
+                />
+                <button onClick={() => onChangeTodo(todo.id)}>Change</button>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+      <h2>Actions</h2>
+      <div className='footer-filter'>
+        <button onClick={handleMarkAll}>Mark All Completed</button>
+        <button onClick={handleClearCompleted}>Clear Completed</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
